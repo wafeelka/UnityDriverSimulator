66b35ac252ee9e8edfc929c527b8b26cb733b1bc
wafeelka <49671882+wafeelka@users.noreply.github.com>
2020-07-05 07:39:29 +0300

    Upload files PlayerController, FollowPlayer.
    FollowPlayer for the main camera
    PlayerController for a car(Or something else)

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // private variables
+    private float speed;
+    private float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //We'll going make a moveController for our Tank
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
